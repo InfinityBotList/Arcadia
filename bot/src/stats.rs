a397b179a7bb57c6698a11c4988a04a903a685b7
Frostpaw <frostpaw@Frostpaws-MacBook-Air.local>
2022-08-09 17:00:30 +0530

    avoid needless borrow

--- bot/src/stats.rs
+++ bot/src/stats.rs
@@ -9,7 +9,7 @@ pub async fn stats(ctx: Context<'_>) -> Result<(), Error> {
             .field("Bot version", crate::VERSION, true)
             .field("libavacado version", libavacado::VERSION, true)
             .field("rustc", libavacado::RUSTC_VERSION, true)
-            .field("Git Commit", libavacado::GIT_SHA.to_string() + "(semver=" + &libavacado::GIT_SEMVER + ")", true)
+            .field("Git Commit", libavacado::GIT_SHA.to_string() + "(semver=" + libavacado::GIT_SEMVER + ")", true)
             //.field("Uptime", format!("{}", chrono::Duration::from_std(std::time::SystemTime::now().duration_since(start_time)).unwrap()), true)
             .field("Commit Message", libavacado::GIT_COMMIT_MSG, true)
             .field("Built On", libavacado::BUILD_CPU, true)

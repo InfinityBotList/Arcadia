3a0bf07eb9954df62e27d865de2fafdaccc9b929
bg <bg@gmail.com>
2022-10-24 18:24:07 +0530

    fix

--- api/src/main.rs
+++ api/src/main.rs
@@ -137,6 +137,7 @@ async fn main() -> std::io::Result<()> {
             .service(routes::get_apps_auth_api)
             .service(routes::perform_apps_auth_api)
             .service(routes::create_app_api)
+            .service(routes::get_app_api)
     })
     .workers(8)
     .bind("localhost:3010")?

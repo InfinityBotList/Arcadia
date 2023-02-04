ebe2bf6436ef6ff1556881e66156e1ab359d178b
Your Name <you@example.com>
2023-02-05 01:03:22 +0530

    add rpc types

--- api/src/models.rs
+++ api/src/models.rs
@@ -3,13 +3,17 @@ use serde::{Deserialize};
 use moka::future::Cache;
 use sqlx::PgPool;
 
+// For frontend API interface generation
+use ts_rs::TS; 
+
 pub struct AppState {
     pub cache_http: CacheHttpImpl,
     pub pool: PgPool,
     pub ratelimits: Cache<String, u64>,
 }
 
-#[derive(Deserialize)]
+#[derive(Deserialize, TS)]
+#[ts(export, export_to="../.generated/RPCRequest.ts")]
 pub struct RPCRequest {
     pub user_id: String,
     pub token: String,
@@ -17,7 +21,8 @@ pub struct RPCRequest {
     pub protocol: u8,
 }
 
-#[derive(Deserialize)]
+#[derive(Deserialize, TS)]
+#[ts(export, export_to="../.generated/RPCMethod.ts")]
 pub enum RPCMethod {
     BotApprove { bot_id: String, reason: String },
     BotDeny { bot_id: String, reason: String },

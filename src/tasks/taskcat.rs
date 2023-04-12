b290dfd5e3be306ec0f8a22243140c7c48520339
Your Name <cheesycod@outlook.com>
2023-04-13 02:02:24 +0530

    reduce time

--- src/tasks/taskcat.rs
+++ src/tasks/taskcat.rs
@@ -54,7 +54,7 @@ async fn taskcat(
         Task::StaffResync => Duration::from_secs(45),
         Task::PremiumRemove => Duration::from_secs(75),
         Task::SpecRoleSync => Duration::from_secs(50),
-        Task::Uptime => Duration::from_secs(900),
+        Task::Uptime => Duration::from_secs(90),
     };
 
     let task_desc = match task {

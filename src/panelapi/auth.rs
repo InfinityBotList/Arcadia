ee9404225f690f291020d9c638dfbded810494e8
cheesycod <cheesycod@outlook.com>
2023-09-09 08:40:31 +0530

    enable botmanagement

--- src/panelapi/auth.rs
+++ src/panelapi/auth.rs
@@ -103,6 +103,7 @@ pub async fn get_capabilities(pool: &PgPool, token: &str) -> Result<Vec<Capabili
 
     if perms.staff {
         capabilities.push(Capability::ViewBotQueue);
+        capabilities.push(Capability::BotManagement);
         capabilities.push(Capability::Rpc);
     }
 

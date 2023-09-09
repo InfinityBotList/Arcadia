29965a5d984ff97e4903d3a69a11091c8380f623
cheesycod <cheesycod@outlook.com>
2023-09-09 23:45:57 +0530

    fix

--- src/config.rs
+++ src/config.rs
@@ -112,7 +112,7 @@ impl Default for Config {
             channels: Channels::default(),
             frontend_url: String::from("https://infinitybots.gg"),
             infernoplex_url: String::from("https://infernoplex.infinitybots.gg"),
-            popplio_url: String::from("http://spider.infinitybots.gg"),
+            popplio_url: String::from("https://spider.infinitybots.gg"),
             proxy_url: String::from("http://127.0.0.1:3219"),
             owners: vec![NonZeroU64::new(510065483693817867).unwrap()],
             protected_bots: vec![

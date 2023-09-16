bd5c50e9254ccd65bb53072d6aa88ca215e0b455
cheesycod <cheesycod@outlook.com>
2023-09-16 13:13:15 +0530

    add partner_types

--- src/impls/partners.rs
+++ src/impls/partners.rs
@@ -21,14 +21,25 @@ pub struct Partner {
 }
 
 #[derive(Serialize, Deserialize, PartialEq, TS, Clone, Default, ToSchema)]
+#[ts(export, export_to = ".generated/PartnerType.ts")]
+pub struct PartnerType {
+    pub id: String,
+    pub name: String,
+    pub short: String,
+    pub icon: String,
+    pub created_at: DateTime<Utc>,
+}
+
+#[derive(Serialize, Deserialize, PartialEq, TS, Clone, Default, ToSchema)]
 #[ts(export, export_to = ".generated/Partners.ts")]
 pub struct Partners {
-    pub partners: Vec<Partner>
+    pub partners: Vec<Partner>,
+    pub partner_types: Vec<PartnerType>,
 }
 
 impl Partners {
     pub async fn fetch(pool: &PgPool) -> Result<Self, Error> {
-        let prec = sqlx::query!("SELECT * FROM partners")
+        let prec = sqlx::query!("SELECT id, name, image, short, links, type, created_at, user_id FROM partners")
             .fetch_all(pool)
             .await?;
 
@@ -49,8 +60,27 @@ impl Partners {
             )
         }
 
+        let ptrec = sqlx::query!("SELECT id, name, short, icon, created_at FROM partner_types")
+            .fetch_all(pool)
+            .await?;
+
+        let mut partner_types = Vec::new();
+        
+        for partner_type in ptrec {
+            partner_types.push(
+                PartnerType {
+                    id: partner_type.id,
+                    name: partner_type.name,
+                    short: partner_type.short,
+                    icon: partner_type.icon,
+                    created_at: partner_type.created_at,
+                }
+            )
+        }
+
         Ok(Self {
-            partners
+            partners,
+            partner_types,
         })
     }
 }

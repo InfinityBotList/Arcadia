9539553453fa24723d7e23f5c12daf22abf9eccf
cheesycod <cheesycod@outlook.com>
2023-11-15 20:20:43 +0530

    fixup team cleaner

--- src/tasks/teamcleaner.rs
+++ src/tasks/teamcleaner.rs
@@ -1,79 +1,117 @@
-use log::info;
+use log::{info, warn};
 
 pub async fn team_cleaner(pool: &sqlx::PgPool) -> Result<(), crate::Error> {
-    // Get all teams with no members
-    let res = sqlx::query!("SELECT team_id FROM team_members")
-        .fetch_all(pool)
+    // Create a transaction
+    let mut tx = pool
+        .begin()
+        .await
+        .map_err(|e| format!("Error creating transaction: {:?}", e))?;
+
+    let res = sqlx::query!("SELECT id FROM teams")
+        .fetch_all(&mut *tx)
         .await
         .map_err(|e| format!("Error while fetching all teams: {}", e))?;
 
     info!("Found {} teams totally", res.len());
 
     for rec in res {
-        let team_id = rec.team_id;
+        let team_id = rec.id;
 
         // Check if team has members
-        let res = sqlx::query!(
+        if sqlx::query!(
             "SELECT COUNT(*) FROM team_members WHERE team_id = $1",
             team_id
         )
-        .fetch_one(pool)
+        .fetch_one(&mut *tx)
         .await
         .map_err(|e| {
             format!(
                 "Error while checking if team {} has members: {}",
                 team_id, e
             )
-        })?;
-
-        let count = res.count.unwrap_or(0);
-
-        if count == 1 {
-            // Ensure team_members perm array has Global Owner in it
-            let tm = sqlx::query!("SELECT flags FROM team_members WHERE team_id = $1", team_id,)
-                .fetch_one(pool)
-                .await
-                .map_err(|e| {
-                    format!(
-                        "Error while checking if team {} has members: {}",
-                        team_id, e
-                    )
-                })?;
-
-            let mut flags: Vec<String> = tm.flags;
-
-            if !flags.contains(&"global.*".to_string()) {
-                // Give them global owner, and add
-                flags.push("global.*".to_string());
-
-                sqlx::query!(
-                    "UPDATE team_members SET flags = $1 WHERE team_id = $2",
-                    &flags,
-                    team_id,
-                )
-                .execute(pool)
-                .await
-                .map_err(|e| format!("Error while updating flags for team {}: {}", team_id, e))?;
-
-                info!(
-                    "Added global.* to flags for team {} due to having only 1 member WITHOUT a Global Owner",
-                    team_id
-                );
-            }
-        }
-
-        if count > 0 {
-            continue;
-        }
-
-        // Delete team
-        sqlx::query!("DELETE FROM teams WHERE id = $1", team_id)
-            .execute(pool)
+        })?.count.unwrap_or(0) == 0 {
+            // Delete team
+            sqlx::query!("DELETE FROM teams WHERE id = $1", team_id)
+            .execute(&mut *tx)
             .await
             .map_err(|e| format!("Error while deleting team {}: {}", team_id, e))?;
 
-        info!("Deleted team {}", team_id);
+            info!("Deleted team {}", team_id);
+            continue;
+        }
+
+        // Ensure team_members perm array has Global Owner in it
+        let tm_with_global_owner = sqlx::query!("SELECT user_id FROM team_members WHERE team_id = $1 AND flags @> ARRAY['global.*']", team_id)
+        .fetch_all(&mut *tx)
+        .await
+        .map_err(|e| {
+            format!(
+                "Error while checking count of team_members with global owner: {}: {}",
+                team_id, e
+            )
+        })?;
+
+        if tm_with_global_owner.is_empty() {
+            let (user_id, _has_dh) = {
+                let dh = sqlx::query!("SELECT user_id FROM team_members WHERE team_id = $1 AND data_holder = true", team_id)
+                .fetch_optional(&mut *tx)
+                .await
+                .map_err(|e| format!("Error while fetching data_holder for team {}: {}", team_id, e))?;
+
+                if let Some(dh) = dh {
+                    (dh.user_id, true)
+                } else {
+                    let res = sqlx::query!("SELECT user_id FROM team_members WHERE team_id = $1 LIMIT 1", team_id)
+                    .fetch_one(&mut *tx)
+                    .await
+                    .map_err(|e| format!("Error while fetching first team member for team {}: {}", team_id, e))?;
+
+                    (res.user_id, false)
+                }
+            };
+
+            sqlx::query!(
+                "UPDATE team_members SET flags = $1, data_holder = $2 WHERE team_id = $3 AND user_id = $4",
+                &["global.*".to_string()],
+                true,
+                team_id,
+                user_id
+            )
+            .execute(&mut *tx)
+            .await
+            .map_err(|e| format!("Error while updating flags for team {}: {}", team_id, e))?;    
+        }
+
+        // Ensure the team has at least one data_holder
+        if sqlx::query!(
+            "SELECT COUNT(*) FROM team_members WHERE team_id = $1 AND data_holder = true",
+            team_id
+        )
+        .fetch_one(&mut *tx)
+        .await
+        .map_err(|e| {
+            format!(
+                "Error while validating data holders of {}: {}",
+                team_id, e
+            )
+        })?.count.unwrap_or(0) == 0 {
+            // Set a team member whose flags contains global.* to data_holder
+            if !tm_with_global_owner.is_empty() {
+                sqlx::query!(
+                    "UPDATE team_members SET data_holder = true WHERE team_id = $1 AND user_id = $2",
+                    team_id,
+                    tm_with_global_owner[0].user_id,
+                )
+                .execute(&mut *tx)
+                .await
+                .map_err(|e| format!("Error while updating data_holder for team {}: {}", team_id, e))?;
+            } else {
+                warn!("Team {} has no data holders and no global owners", team_id);
+            }
+        }
     }
+
+    tx.commit().await.map_err(|e| format!("Error while committing transaction: {:?}", e))?;
 
     Ok(())
 }

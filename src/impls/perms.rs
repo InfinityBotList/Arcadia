46b204387fb134013ea65ff67221738756553a56
cheesycod <cheesycod@outlook.com>
2023-12-18 18:00:41 +0530

    negators

--- src/impls/perms.rs
+++ src/impls/perms.rs
@@ -7,6 +7,10 @@
 ///
 /// If a permission has no namespace, it will be assumed to be global
 /// 
+/// If a permission has ~ in the beginning of its namespace, it is a negator permission that removes that specific permission from the user
+/// 
+/// Negators work to negate a specific permission *excluding the global.* permission*
+/// 
 /// Anything past the <namespace>.<permission> may be ignored or indexed at the discretion of the implementation and is *undefined behaviour*
 
 /// Check if the user has a permission given a set of user permissions and a permission to check for
@@ -21,7 +25,14 @@ pub fn has_perm(perms: &Vec<String>, perm: &str) -> bool {
     let perm_namespace = perm_split[0];
     let perm_name = perm_split[1];
 
+    let mut has_perm = None;
+    let mut has_negator = false;
     for user_perm in perms {
+        if user_perm == "global.*" {
+            // Special case
+            return true;
+        }
+
         let mut user_perm_split = user_perm.split('.').collect::<Vec<&str>>();
 
         if user_perm_split.len() < 2 {
@@ -29,15 +40,25 @@ pub fn has_perm(perms: &Vec<String>, perm: &str) -> bool {
             user_perm_split = vec![user_perm, "*"];
         }
 
-        let user_perm_namespace = user_perm_split[0];
+        let mut user_perm_namespace = user_perm_split[0];
         let user_perm_name = user_perm_split[1];
 
+        if user_perm.starts_with('~') {
+            // Strip the ~ from namespace to check it
+            user_perm_namespace = user_perm_namespace.trim_start_matches('~');
+        }
+
         if (user_perm_namespace == perm_namespace || user_perm_namespace == "global" || perm_namespace == "global") && (user_perm_name == "*" || user_perm_name == perm_name) {
-            return true;
+            // We have to check for all negator
+            has_perm = Some(user_perm_split);
+
+            if user_perm.starts_with('~') {
+                has_negator = true; // While we can optimize here by returning false, we may want to add more negation systems in the future
+            }
         }
     }
 
-    false
+    has_perm.is_some() && !has_negator
 }
 
 /// Builds a permission string from a namespace and permission
@@ -60,5 +81,12 @@ mod tests {
         assert!(!has_perm(&vec!["apps.*".to_string()], "rpc.ViewBotQueue")); // apps.* does not imply rpc.ViewBotQueue, apps != rpc
         assert!(!has_perm(&vec!["apps.*".to_string()], "rpc.*")); // apps.* does not imply rpc.*, apps != rpc despite the global permission
         assert!(!has_perm(&vec!["apps.test".to_string()], "rpc.test")); // apps.test does not imply rpc.test, apps != rpc despite the permissions being the same
+        
+        // Negator tests
+        assert!(has_perm(&vec!["apps.*".to_string()], "apps.test")); // apps.* implies apps.test
+        assert!(!has_perm(&vec!["~apps.*".to_string()], "apps.test")); // ~apps.* does not imply apps.test as it is negated
+        assert!(!has_perm(&vec!["apps.*".to_string(), "~apps.test".to_string()], "apps.test")); // apps.* does not imply apps.test due to negator ~apps.test
+        assert!(!has_perm(&vec!["~apps.test".to_string(), "apps.*".to_string()], "apps.test")); // apps.* does not imply apps.test due to negator ~apps.test. Same as above with different order of perms to test for ordering
+        assert!(has_perm(&vec!["apps.test".to_string()], "apps.test")); // ~apps.* does not imply apps.test as it is negated
     }
 }

ed7a5ee568941bae6dc7e44d0e30b8f4f868723f
cheesycod <cheesycod@outlook.com>
2023-12-29 17:34:45 +0530

    breaking api changes, group all authorize actions under one operation

--- src/panelapi/types/auth.rs
+++ src/panelapi/types/auth.rs
@@ -1,6 +1,73 @@
 use serde::{Deserialize, Serialize};
 use ts_rs::TS;
+use strum_macros::{Display, EnumString, EnumVariantNames};
 use utoipa::ToSchema;
+
+#[derive(
+    Serialize,
+    Deserialize,
+    ToSchema,
+    TS,
+    EnumString,
+    EnumVariantNames,
+    Display,
+    Clone,
+    PartialEq,
+)]
+#[ts(export, export_to = ".generated/AuthorizeAction.ts")]
+pub enum AuthorizeAction {
+    /// Begin begins an authorization request
+    /// 
+    /// Currently only returns a scope and the login url
+    Begin {
+        /// Scope of the panel. This is a short identifier to ensure a valid arcadia instance
+        scope: String,
+        /// Login protocol version, should be `STARTAUTH_VERSION`
+        version: u16,
+        /// Redirect URL
+        redirect_url: String,
+    },
+    
+    /// CreateSession creates a new 'pending' session for the staff member returning a login token
+    /// 
+    /// Note that MFA/other login methods are needed to 'activate' the session
+    CreateSession {
+        /// Discord OAuth2 code
+        code: String,
+        /// Redirect URL
+        redirect_url: String,
+    },
+
+    /// CheckMFA checks and returns any needed/useful MFA-related information
+    /// 
+    /// This is the only endpoint that works on both pending and active sessions
+    CheckMfaState {
+        /// Login Token
+        login_token: String,
+    },
+
+    /// Resets MFA for a user identified by login token
+    ResetMfaTotp {
+        /// Login token
+        login_token: String,
+        /// Old MFA code
+        otp: String,
+    },
+
+    /// ActivateSession activates a session for a given login token
+    ActivateSession {
+        /// Login token
+        login_token: String,
+        /// MFA code
+        otp: String,
+    },
+
+    /// Logout logs out a session
+    Logout {
+        /// Login token
+        login_token: String,
+    },
+}
 
 /// MFA Login Secret Data
 #[derive(Serialize, Deserialize, TS, ToSchema, Clone)]

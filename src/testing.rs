608a8553c86ac38991be964af05d48f8187736cf
Olivia <ceo@purrquinox.com>
2024-04-16 03:34:58 +0000

    Kick on approve/deny

--- src/testing.rs
+++ src/testing.rs
@@ -460,6 +460,22 @@ pub async fn approve(
         format!("Approved bot!\nPlease invite the bot, to the Caching Server provided down below!\n{}", content)
     ).await?;
 
+    // Check if bot is still in testing server.
+    let member_exists_in_test_server = user_data
+        .cache_http
+        .cache
+        .member(config::CONFIG.servers.testing, bot.user_id.to_string())
+        .is_some();
+
+    if member_exists_in_test_server {
+        // If so, kick them from test server as no longer needed.
+        config::CONFIG
+            .servers
+            .testing
+            .kick_with_reason(&user_data.cache_http.http, new_member.user.id, "Approved!")
+            .await?;
+    }
+
     Ok(())
 }
 
@@ -497,7 +513,23 @@ pub async fn deny(
     })
     .await?;
 
-    ctx.say("Denied bot").await?;
+    ctx.say("Okay! The bot has been denied.").await?;
+
+    // Check if bot is still in testing server.
+    let member_exists_in_test_server = user_data
+        .cache_http
+        .cache
+        .member(config::CONFIG.servers.testing, bot.user_id.to_string())
+        .is_some();
+
+    if member_exists_in_test_server {
+        // If so, kick them from test server as no longer needed.
+        config::CONFIG
+            .servers
+            .testing
+            .kick_with_reason(&user_data.cache_http.http, new_member.user.id, "Denied!")
+            .await?;
+    }
 
     Ok(())
 }
